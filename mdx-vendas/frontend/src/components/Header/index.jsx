ba0f94d93e92705a4a945f3063a310e77f1107ff
Hemrique <henriquejoshua63@gmail.com>
2025-10-06 17:47:32 -0300

    first commit

---
+++ mdx-vendas/frontend/src/components/Header/index.jsx
@@ -0,0 +1,39 @@
+import './Header.css'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            {/* Logo */}
+            <div className='logo'>
+                <Link to='/'>
+                    <span className='logo-1'>H</span>
+                    <span className='logo-2'>J</span>
+                    <span className='logo-3'>L</span>
+                </Link>
+            </div>
+
+            {/* Busca */}
+            <div className='search-bar'>
+                <input type='text' placeholder='Buscar "apartamento"' />
+                <div className='location'>
+                    <button className='search-btn'>
+                    <ion-icon name="search-outline"></ion-icon>
+                    </button>
+                </div>
+            </div>
+
+            {/* Botões */}
+            <nav className='navbar'>
+                <ul>
+                    <li>
+                        <Link to='/signin' className='entrarBtn'>Entrar</Link>
+                    </li>
+                    <li>
+                    <Link to='/' className='anunciarBtn'>Anunciar gratis</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
